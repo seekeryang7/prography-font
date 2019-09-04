c1b1457c0f7a2183c964341b41a616439cb29cec
seekeryang7 <seekeryang7@gmail.com>
2019-09-04 18:19:23 +0900

    final

--- src/App.js
+++ src/App.js
@@ -2,33 +2,32 @@ import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
-
+import './App.css';
 
 class App extends Component {
-
-  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+  id = 3 
 
   state = {
     input: '',
     todos: [
       { id: 0, text: "프로그라피 지원하기", checked: false },
-      { id: 1, text: "프로그라피 과제 준비하기", checked: true },
+      { id: 1, text: "프로그라피 과제 준비하기", checked: false },
       { id: 2, text: "프로그라피 친구들한테 알려주기", checked: false },
       { id: 3, text: "사랑해요 프로그라피", checked: false }
-    ]
+    ],
+    fold: false
   }
 
   handleChange = (e) => {
     this.setState({
-      input: e.target.value // input 의 다음 바뀔 값
+      input: e.target.value 
     });
   }
 
   handleCreate = () => {
     const { input, todos } = this.state;
     this.setState({
-      input: '', // 인풋 비우고
-      // concat 을 사용하여 배열에 추가
+      input: '', 
       todos: todos.concat({
         id: this.id++,
         text: input,
@@ -38,7 +37,6 @@ class App extends Component {
   }
 
   handleKeyPress = (e) => {
-    // 눌려진 키가 Enter 면 handleCreate 호출
     if(e.key === 'Enter') {
       this.handleCreate();
     }
@@ -47,13 +45,9 @@ class App extends Component {
   handleToggle = (id) => {
     const { todos } = this.state;
     
-    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
     const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index]; // 선택한 객체
-
-    const nextTodos = [...todos]; // 배열을 복사
-    
-    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    const selected = todos[index];
+    const nextTodos = [...todos];    
     nextTodos[index] = { 
       ...selected, 
       checked: !selected.checked
@@ -90,7 +84,8 @@ class App extends Component {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+        <button className="toggle-button" onClick={() => this.setState({ fold: !this.state.fold})}>{this.state.fold ? "펼치기" : "접기" }</button>
+        {!this.state.fold ? <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/> : null}
       </TodoListTemplate>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,7 +13,7 @@ class TodoItem extends Component {
     return (
       <div className="todo-item" onClick={() => onToggle(id)}>
         <div className="remove" onClick={(e) => {
-          e.stopPropagation(); // onToggle 이 실행되지 않도록 함
+          e.stopPropagation();
           onRemove(id)}
         }>&times;</div>
         <div className={`todo-text ${checked && 'checked'}`}>
